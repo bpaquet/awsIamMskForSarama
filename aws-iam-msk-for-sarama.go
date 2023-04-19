7d2031861bfdfceb2c7763fcae3ff55b1ed4eab5
Bertrand Paquet <bertrand.paquet@gmail.com>
2023-04-19 23:03:25 +0200

    Implement a custom SASL mechanism for AWS MSK

---
+++ aws-iam-msk-for-sarama.go
@@ -0,0 +1,40 @@
+package awsIamMskForSarama
+
+import (
+	"github.com/Shopify/sarama"
+)
+
+type GeneratorFunc struct {
+	AwsRegion string
+	addr      string
+	userAgent string
+	done      bool
+}
+
+func (x *GeneratorFunc) Begin(addr string, config *sarama.Config) error {
+	x.addr = addr
+	x.userAgent = "awsIamMskForSarama"
+	x.done = false
+	return nil
+}
+
+func (x *GeneratorFunc) Step(challenge string) (string, error) {
+	if challenge == "" {
+		response, err := buildAwsIAMPayload(x.addr, x.userAgent, AWSMSKIAMConfig{Region: x.AwsRegion})
+		if err != nil {
+			return "", err
+		}
+		return string(response), nil
+	}
+	return "", nil
+}
+
+func (x *GeneratorFunc) Done() bool {
+	result := x.done
+	x.done = true
+	return result
+}
+
+func (x *GeneratorFunc) MechanismName() string {
+	return "AWS_MSK_IAM"
+}
